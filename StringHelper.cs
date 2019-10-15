97fdf48ec19d2da834ef0f0e456994c8f64e47c3
lucynao <lucynaoch@gmail.com>
2019-10-15 11:18:55 +0200

    new function added CountingPunctuationMark

--- StringHelper.cs
+++ StringHelper.cs
@@ -5,13 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Panda3Projekt1
+namespace Panda3
 {
     /// <summary>
     /// helper class for operatios on text
     /// </summary>
     public class StringHelper
     {
-
+        /// <summary>
+        /// function to count punctional marks in text
+        /// problem with ...
+        /// </summary>
+        public static int CountPunctuationMark(string text)
+        {
+            char[] chars = text.ToCharArray();
+            int numberOfPunctuationMarks = 0;
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if(char.IsPunctuation(chars[i])==true)
+                {
+                    numberOfPunctuationMarks = numberOfPunctuationMarks + 1;
+                }
+            }
+           
+            return numberOfPunctuationMarks;
+        }
     }
 }
