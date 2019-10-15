2e24582baeb4aaa1a2c72e0f3d63df9b0ac6733e
lucynao <lucynaoch@gmail.com>
2019-10-15 10:42:54 +0200

    adding new class for operation on text

---
+++ StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Panda3Projekt1
+{
+    /// <summary>
+    /// helper class for operatios on text
+    /// </summary>
+    public class StringHelper
+    {
+
+    }
+}
