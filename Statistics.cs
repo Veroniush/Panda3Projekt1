61c21ba5ea88f17a0d7ed6577db9ed93d12a0b62
lucynao <lucynaoch@gmail.com>
2019-10-16 09:55:41 +0200

    functionality to write statistics to file

---
+++ Statistics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda3
+{
+    public class Statistics
+    {
+        /// <summary>
+        /// functionality to write statistics in file statystiki.txt
+        /// </summary>
+        public static void CreateStatistic(string text)
+        {
+            List<string> lines = new List<string>();
+
+            if(text == null)
+            {
+                lines.Add("File doesn't exist");
+            }
+            else
+            {
+                lines.Add("Number of letters: " + StringHelper.CountLetters(text).ToString());
+                lines.Add("Number of words: " + StringHelper.CountWords(text).ToString());
+                lines.Add("Numer of punctuation marks: " + StringHelper.CountPunctuationMark(text).ToString());
+                lines.Add("Numer of sentenses: " + StringHelper.CountSentenses(text).ToString());
+
+            }
+
+            File.WriteAllLines("statystyki.txt", lines);
+        }
+    }
+}
