efab675a3b9468d62ee2f528c75f400fe3f4ae3e
lucynao <44733630+lucynao@users.noreply.github.com>
2019-10-23 20:49:44 +0200

    refactoring and fixes (#44)

--- StringHelper.cs
+++ StringHelper.cs
@@ -13,7 +13,7 @@ namespace Panda3
     public class StringHelper
     {
         /// <summary>
-        /// function to count punctional marks in text
+        /// function to count punctional marks ('.' and '?' only) in text
         /// </summary>
         public static int CountPunctuationMark(string text)
         {
@@ -21,25 +21,10 @@ namespace Panda3
             int numberOfPunctuationMarks = 0;
             for (int i = 0; i < chars.Length; i++)
             {
-                if (char.IsPunctuation(chars[i]) == true)
-                {
-                    if (chars[i] == '.' && i < chars.Length - 2)
-                    {
-                        if (chars[i + 1] == '.' && chars[i + 2] == '.')
-                        {
-                            numberOfPunctuationMarks = numberOfPunctuationMarks + 1;
-                            i = i + 2;
-                        }
-                        else
-                        {
-                            numberOfPunctuationMarks = numberOfPunctuationMarks + 1;
-                        }
-                    }
-                    else
-                    {
-                        numberOfPunctuationMarks = numberOfPunctuationMarks + 1;
-                    }
-                }
+               if(chars[i] == '.' || chars[i] == '?')
+               {
+                    numberOfPunctuationMarks++;
+               }           
             }
             return numberOfPunctuationMarks;
         }
